a379d3c723b6bfd42198db1abeec44e77227e093
小张学Python <44359146+energy888666@users.noreply.github.com>
2021-04-06 14:37:03 +0800

    Add files via upload

---
+++ dfdnet.bat
@@ -0,0 +1,2 @@
+python test_FaceDict1-2GPUorCPU.py --test_path ./TestData/TestWhole --results_dir ./Results/TestWholeResults --upscale_factor 4 --gpu_ids 0
+python test_FaceDict3-4CPUOnly.py --test_path ./TestData/TestWhole --results_dir ./Results/TestWholeResults --upscale_factor 4 --gpu_ids -1
